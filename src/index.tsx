11e9450f85e9ac37816976613b2d378667c02918
dina <dinaisakova10@gmail.com>
2026-06-26 07:32:31 +0900

    feat: create routing system

--- src/index.tsx
+++ src/index.tsx
@@ -7,14 +7,17 @@ import App from "./app/App";
 import CssBaseline from "@mui/material/CssBaseline";
 import { ThemeProvider } from "@mui/material/styles";
 import theme from "./app/MaterialTheme";
-import "./css/index.css"; // Mana shu yer css papkasiga to'g'rilandi!
+import "./css/index.css";
+import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <App />
+        <Router>
+          <App />
+        </Router>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
