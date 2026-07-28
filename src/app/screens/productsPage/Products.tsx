44415fe399c8ac327e8f4eec79ee547a9969ce16
Dina-me10 <dinaisakova10@gmail.com>
2026-07-28 15:32:25 +0900

    feat: develop logic of adding products into basket

--- src/app/screens/productsPage/Products.tsx
+++ src/app/screens/productsPage/Products.tsx
@@ -19,6 +19,7 @@ import ProductService from "../../services/ProductService";
 import { ProductCollection, ProductVolume } from "../../../lib/enums/product.enum";
 import { serverApi } from "../../../lib/config";
 import { useHistory } from "react-router-dom";
+import { CartItem } from "../../../lib/types/search";
 
 /** REDUX SLICE & SELECTOR **/
 const actionDispatch = (dispatch: Dispatch) => ({
@@ -28,9 +29,12 @@ const productsRetriever = createSelector(retrieveProducts, (products) => ({
   products,
 }));
 
+interface ProductsProps {
+  onAdd: (item: CartItem) => void;
+}
 
-
-export default function Products() {
+export default function Products(props: ProductsProps) {
+  const { onAdd } = props;
  const { setProducts } = actionDispatch(useDispatch());
  const {products } = useSelector(productsRetriever);
  const [productSearch, setProductSearch] = useState<ProductInquiry>({
@@ -210,12 +214,25 @@ const chooseDishHandler = (id: string ) => {
                         sx={{ backgroundImage: `url(${imagePath})` }}
                       >
                         <div className={"product-sale"}>{sizeVolume}</div>
-                        <Button className={"shop-btn"}>
-                          <img
-                            src={"/icons/shopping-cart.svg"}
-                            style={{ display: "flex" }}
-                          />
-                        </Button>
+                        <Button
+  className={"shop-btn"}
+  onClick={(e) => {
+    console.log("BUTTON PRESSED!");
+    onAdd({
+  _id: product._id,
+  quantity: 1,
+  name: product.productName,
+  price: product.productPrice,
+  image: product.productImages[0],
+});
+    e.stopPropagation();
+  }}
+>
+  <img
+    src={"/icons/shopping-cart.svg"}
+    style={{ display: "flex" }}
+  />
+</Button>
                         <Button className={"view-btn"} sx={{ right: "36px" }}>
                           <Badge badgeContent={product.productViews} color="secondary">
                             <RemoveRedEyeIcon
