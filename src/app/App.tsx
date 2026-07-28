0d78a37d3db87607909cc19f0f207a54b10e24a9
Dina-me10 <dinaisakova10@gmail.com>
2026-07-28 17:48:19 +0900

    feat: create signup process

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -15,65 +15,70 @@ import Test from "./screens/Test";
 import { CartItem } from "../lib/types/search";
 import useBasket from "./hooks/useBasket";
 import AuthenticationModal from "./components/auth";
-
+ 
 function App() {
   const location = useLocation();
-  const {cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
+  const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = useBasket();
   const [signupOpen, setSignupOpen] = useState<boolean>(false);
-const [loginOpen, setLoginOpen] = useState<boolean>(false);
-
-/** HANDLERS **/
-
-const handleSignupClose = () => setSignupOpen(false);
-const handleLoginClose = () => setLoginOpen(false);
-
-return (
-  <>
-    {location.pathname === "/" ? (
-      <HomeNavbar
-        cartItems={cartItems}
-        onAdd={onAdd}
-        onRemove={onRemove}
-        onDelete={onDelete}
-        onDeleteAll={onDeleteAll}
+  const [loginOpen, setLoginOpen] = useState<boolean>(false);
+ 
+  /** HANDLERS **/
+ 
+  const handleSignupClose = () => setSignupOpen(false);
+  const handleLoginClose = () => setLoginOpen(false);
+ 
+  return (
+    <>
+      {location.pathname === "/" ? (
+        <HomeNavbar
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
+        />
+      ) : (
+        <OtherNavbar
+          cartItems={cartItems}
+          onAdd={onAdd}
+          onRemove={onRemove}
+          onDelete={onDelete}
+          onDeleteAll={onDeleteAll}
+          setSignupOpen={setSignupOpen}
+          setLoginOpen={setLoginOpen}
+        />
+      )}
+ 
+      <Switch>
+        <Route path="/products">
+          <ProductsPage onAdd={onAdd} />
+        </Route>
+        <Route path="/orders">
+          <OrdersPage />
+        </Route>
+        <Route path="/member-page">
+          <UserPage />
+        </Route>
+        <Route path="/help">
+          <HelpPage />
+        </Route>
+        <Route path="/">
+          <HomePage />
+        </Route>
+      </Switch>
+      <Footer />
+ 
+      <AuthenticationModal
+        signupOpen={signupOpen}
+        loginOpen={loginOpen}
+        handleLoginClose={handleLoginClose}
+        handleSignupClose={handleSignupClose}
       />
-    ) : (
-      <OtherNavbar
-        cartItems={cartItems}
-        onAdd={onAdd}
-        onRemove={onRemove}
-        onDelete={onDelete}
-        onDeleteAll={onDeleteAll}
-      />
-    )}
-
-  <Switch>
-  <Route path="/products">
-    <ProductsPage onAdd={onAdd} />
-  </Route>
-  <Route path="/orders">
-    <OrdersPage />
-  </Route>
-  <Route path="/member-page">
-    <UserPage />
-  </Route>
-  <Route path="/help">
-    <HelpPage />
-  </Route>
-  <Route path="/">
-    <HomePage />
-  </Route>
-</Switch>
-<Footer />
-
- <AuthenticationModal
-  signupOpen={signupOpen}
-  loginOpen={loginOpen}
-  handleLoginClose={handleLoginClose}
-  handleSignupClose={handleSignupClose}
-/>
-  </>
-);
+    </>
+  );
 }
-
-export default App; 
+ 
+export default App;
+ 
